06e6dca1ade8e84028e6ffc823da3f173ac9ec45
CristhianCanar <cristhian.canar.m@gmail.com>
2023-03-17 09:55:26 -0500

    Se adelanta un modelo extra, se adelanta registro

--- app/Http/Controllers/Api/AuthApiController.php
+++ app/Http/Controllers/Api/AuthApiController.php
@@ -3,12 +3,64 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Tymon\JWTAuth\Exceptions\JWTException;
 use Tymon\JWTAuth\Facades\JWTAuth;
 
 class AuthApiController extends Controller
 {
+    public function register(Request $request)
+    {
+        /* VAL
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6|confirmed',
+        ]);
+
+        if($validator->fails()){
+                return response()->json($validator->errors()->toJson(), 400);
+        }
+        */
+        $nombre     = $request->name;
+        $apellido   = $request->lastname;
+        $user = User::create([
+                    // El rol sera 4 = Cliente
+                    'rol_id'            => 4,
+                    'nombre'            => $nombre,
+                    'apellido'          => $apellido,
+                    'email'             => $request->email,
+                    'password'          => Hash::make($request->password),
+                    'telefono'          => "321",
+                    'medio_registro'    => "API-MOVIL"
+                ]);
+        $user_json = [
+            "user" => [
+                "__v" => 0,
+                "_id" => $user->id_user,
+                "blocked" => $user->bloqueado,
+                "confirmed" => $user->verificado,
+                "createdAt" => $user->created_at,
+                "email" => $user->email,
+                "id" => $user->id_user,
+                "provider" => "local",
+                "role" => [
+                  "__v" => 0,
+                  "_id" => $user->rol_id,
+                  "description" => $user->rol->descripcion,
+                  "id" =>  $user->rol_id,
+                  "name" => $user->rol->rol,
+                  "type" => $user->rol->rol,
+                ],
+                "services" => [],
+                "updatedAt" => $user->updated_at,
+                "username" => $user->email,
+            ]
+        ];
+        return response()->json($user_json);
+    }
     public function signIn(Request $request)
     {
         $credentials = ['email' => $request->correo,'password' => $request->contrasenia];
